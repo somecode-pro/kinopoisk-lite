61427403f8a980ac0f59aa8a045aef431792edbc
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 16:52:30 +0300

    Добавление и использование компонентов

--- views/pages/movies.php
+++ views/pages/movies.php
@@ -1,13 +1,3 @@
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/assets/css/app.css">
-</head>
-<body>
+<?php include __DIR__.'/../components/start.php' ?>
 <h1>Movies page</h1>
-</body>
-</html>
+<?php include __DIR__.'/../components/end.php' ?>
