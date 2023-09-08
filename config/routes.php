63cab6809dcabe46e267fdf368af4c3e227752ac
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 13:51:49 +0300

    Добавил класс Route

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     '/home' => function () {
         include_once APP_PATH.'/views/pages/home.php';
     },
---
+++ src/Router/Route.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Router;
+
+class Route
+{
+    public function __construct(
+        private string $uri,
+        private string $method,
+        private $action
+    ) {
+    }
+
+    public static function get(string $uri, $action): static
+    {
+        return new static($uri, 'GET', $action);
+    }
+
+    public static function post(string $uri, $action): static
+    {
+        return new static($uri, 'POST', $action);
+    }
+
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    public function getAction(): callable
+    {
+        return $this->action;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+}
