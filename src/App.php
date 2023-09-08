f30e70fd1a04c489e9d2be17dd124ac7d54ed66e
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 14:34:37 +0300

    Добавил класс Request

--- src/App.php
+++ src/App.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Http\Request;
 use App\Router\Router;
 
 class App
@@ -9,10 +10,8 @@ class App
     public function run(): void
     {
         $router = new Router();
+        $request = Request::createFromGlobals();
 
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        $router->dispatch($uri, $method);
+        $router->dispatch($request->uri(), $request->method());
     }
 }
---
+++ src/Http/Request.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http;
+
+class Request
+{
+    public function __construct(
+        public readonly array $get,
+        public readonly array $post,
+        public readonly array $server,
+        public readonly array $files,
+        public readonly array $cookies,
+    ) {
+    }
+
+    public static function createFromGlobals(): static
+    {
+        return new static($_GET, $_POST, $_SERVER, $_FILES, $_COOKIE);
+    }
+
+    public function uri(): string
+    {
+        return strtok($this->server['REQUEST_URI'], '?');
+    }
+
+    public function method(): string
+    {
+        return $this->server['REQUEST_METHOD'];
+    }
+}
