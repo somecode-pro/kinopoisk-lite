4fe0a70e601f061bbb461ece89866d342e492b49
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-09 11:43:34 +0300

    Добавил класс Validator

---
+++ kernel/Validator/Validator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Kernel\Validator;
+
+class Validator
+{
+    private array $errors = [];
+
+    private array $data;
+
+    public function validate(array $data, array $rules): bool
+    {
+        $this->errors = [];
+        $this->data = $data;
+
+        foreach ($rules as $key => $rule) {
+            $rules = $rule;
+
+            foreach ($rules as $rule) {
+                $rule = explode(':', $rule);
+
+                $ruleName = $rule[0];
+                $ruleValue = $rule[1] ?? null;
+
+                $error = $this->validateRule($key, $ruleName, $ruleValue);
+
+                if ($error) {
+                    $this->errors[$key][] = $error;
+                }
+            }
+        }
+
+        return empty($this->errors);
+    }
+
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+
+    private function validateRule(string $key, string $ruleName, string $ruleValue = null): string|false
+    {
+        $value = $this->data[$key];
+
+        switch ($ruleName) {
+            case 'required':
+                if (empty($value)) {
+                    return 'Field is required';
+                }
+                break;
+            case 'min':
+                if (strlen($value) < $ruleValue) {
+                    return "Field must be at least $ruleValue characters long";
+                }
+                break;
+            case 'max':
+                if (strlen($value) > $ruleValue) {
+                    return "Field must be at most $ruleValue characters long";
+                }
+                break;
+            case 'email':
+                if (! filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                    return 'Field must be a valid email address';
+                }
+                break;
+        }
+
+        return false;
+    }
+}
