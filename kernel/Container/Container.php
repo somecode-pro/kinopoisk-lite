e9cfe72217d86b679a535a1596934c1666d3a346
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-09 12:04:34 +0300

    Внедрение валидатора в Request

--- kernel/Container/Container.php
+++ kernel/Container/Container.php
@@ -4,6 +4,7 @@ namespace App\Kernel\Container;
 
 use App\Kernel\Http\Request;
 use App\Kernel\Router\Router;
+use App\Kernel\Validator\Validator;
 use App\Kernel\View\View;
 
 class Container
@@ -13,6 +14,8 @@ class Container
     public readonly Router $router;
 
     public readonly View $view;
+
+    public readonly Validator $validator;
 
     public function __construct()
     {
@@ -24,5 +27,7 @@ class Container
         $this->request = Request::createFromGlobals();
         $this->view = new View();
         $this->router = new Router($this->view, $this->request);
+        $this->validator = new Validator();
+        $this->request->setValidator($this->validator);
     }
 }
--- kernel/Http/Request.php
+++ kernel/Http/Request.php
@@ -2,8 +2,12 @@
 
 namespace App\Kernel\Http;
 
+use App\Kernel\Validator\Validator;
+
 class Request
 {
+    private Validator $validator;
+
     public function __construct(
         public readonly array $get,
         public readonly array $post,
@@ -31,5 +35,26 @@ class Request
     public function input(string $key, $default = null): mixed
     {
         return $this->post[$key] ?? $this->get[$key] ?? $default;
+    }
+
+    public function setValidator(Validator $validator): void
+    {
+        $this->validator = $validator;
+    }
+
+    public function validate(array $rules): bool
+    {
+        $data = [];
+
+        foreach ($rules as $field => $rule) {
+            $data[$field] = $this->input($field);
+        }
+
+        return $this->validator->validate($data, $rules);
+    }
+
+    public function errors(): array
+    {
+        return $this->validator->errors();
     }
 }
