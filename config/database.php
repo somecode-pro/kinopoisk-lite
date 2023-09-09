c91d7c29f1a8ee573289ff136e71328fccb928e6
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-09 13:18:57 +0300

    Добавил конфиг для базы данных

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'php-mvc',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
