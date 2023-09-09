87b5adc3abd771aa51a918431fb93d0b85ba2660
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-09 14:06:40 +0300

    Форма авторизации

---
+++ src/Controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Kernel\Controller\Controller;
+
+class LoginController extends Controller
+{
+    public function index(): void
+    {
+        $this->view('login');
+    }
+}
