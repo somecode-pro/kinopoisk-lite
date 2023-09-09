92b8ed7f23de779364c3d67e77153f5a8a9b814c
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-09 10:55:59 +0300

    Проверка существования view шаблона

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -6,6 +6,12 @@ class View
 {
     public function page(string $name): void
     {
+        $viewPath = APP_PATH."/views/pages/$name.php";
+
+        if (! file_exists($viewPath)) {
+            throw new \Exception("View $name not found");
+        }
+
         extract([
             'view' => $this,
         ]);
