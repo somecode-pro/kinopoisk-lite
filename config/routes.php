7bae55726a2c8f8afbd9aaae89ca966ac0453d99
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-10 17:13:38 +0300

    Добавил посредник GuestMiddleware

--- config/routes.php
+++ config/routes.php
@@ -6,15 +6,16 @@ use App\Controllers\MovieController;
 use App\Controllers\RegisterController;
 use App\Kernel\Router\Route;
 use App\Middleware\AuthMiddleware;
+use App\Middleware\GuestMiddleware;
 
 return [
     Route::get('/home', [HomeController::class, 'index']),
     Route::get('/movies', [MovieController::class, 'index']),
     Route::get('/admin/movies/add', [MovieController::class, 'add'], [AuthMiddleware::class]),
     Route::post('/admin/movies/add', [MovieController::class, 'store']),
-    Route::get('/register', [RegisterController::class, 'index']),
+    Route::get('/register', [RegisterController::class, 'index'], [GuestMiddleware::class]),
     Route::post('/register', [RegisterController::class, 'register']),
-    Route::get('/login', [LoginController::class, 'index']),
+    Route::get('/login', [LoginController::class, 'index'], [GuestMiddleware::class]),
     Route::post('/login', [LoginController::class, 'login']),
     Route::post('/logout', [LoginController::class, 'logout']),
 ];
